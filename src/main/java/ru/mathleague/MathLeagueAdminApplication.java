97d1a8ec0cea0d3e1297bd02836e5b1f4b9be8ea
Ivan <idfedorov09@gmail.com>
2023-06-28 19:52:11 +0300

    init

---
+++ src/main/java/ru/mathleague/MathLeagueAdminApplication.java
@@ -0,0 +1,13 @@
+package ru.mathleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathLeagueAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathLeagueAdminApplication.class, args);
+	}
+
+}
