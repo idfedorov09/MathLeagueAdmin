10ca9144a0ec1ab6411a2020483a33c381d49998
Ivan <idfedorov09@gmail.com>
2023-07-01 01:21:32 +0300

    HTTP->HTTPS BACK!!

--- src/main/java/ru/mathleague/config/HttpHttpsConfig.java
+++ src/main/java/ru/mathleague/config/HttpHttpsConfig.java
@@ -10,49 +10,6 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class HttpHttpsConfig {
 
-    @Value(value = "${server.ssl.key-store:}")
-    private String sslKeyStore;
-    @Value(value = "${server.http.port:80}")
-    private int httpPort;
-    @Value("${server.port:443}")
-    int httpsPort;
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        boolean needRedirectToHttps = sslKeyStore != null && !sslKeyStore.isEmpty();
-
-        TomcatServletWebServerFactory tomcat = null;
-
-        if (!needRedirectToHttps) {
-            tomcat = new TomcatServletWebServerFactory();
-            return tomcat;
-        }
-
-        tomcat = new TomcatServletWebServerFactory() {
-
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(httpPort);
-        connector.setSecure(false);
-        connector.setRedirectPort(httpsPort);
-        return connector;
-    }
 }
