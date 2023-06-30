4c05795f0be9b2284352f5d63c3e5b94f42aad0a
Ivan <idfedorov09@gmail.com>
2023-07-01 01:32:06 +0300

    http https upd

--- src/main/java/ru/mathleague/config/HttpHttpsConfig.java
+++ src/main/java/ru/mathleague/config/HttpHttpsConfig.java
@@ -1,15 +1,4 @@
 package ru.mathleague.config;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 public class HttpHttpsConfig {
-
 }
